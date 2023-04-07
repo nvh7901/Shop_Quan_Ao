1510c345af448852592798b4f0461057b9c90aad
VanHuy79 <ngo.huy.van.2001@gmail.com>
2023-04-07 23:18:04 +0700

    update

--- Fashi_eShop/resources/views/backend/user/create.blade.php
+++ Fashi_eShop/resources/views/backend/user/create.blade.php
@@ -11,34 +11,37 @@
                         @include('backend.components.notification')
                         <form method="post" action="admin/user" enctype="multipart/form-data">
                             @csrf
+                            
                             @error('image')
                                 <div class="alert alert-danger" role="alert">
                                     {{ $message }}
                                 </div>
                             @enderror
                             <div class="position-relative row form-group">
-                                <label for="image" class="col-md-3 text-md-right col-form-label">Avatar</label>
+                                <label for="image" class="col-md-3 text-md-right col-form-label">Hình ảnh</label>
                                 <div class="col-md-9 col-xl-8">
                                     <img style="height: 200px; cursor: pointer;" class="thumbnail rounded-circle"
                                         data-toggle="tooltip" title="Click to change the image" data-placement="bottom"
                                         src="backend/assets/images/add-image-icon.jpg" alt="Avatar">
                                     <input name="image" type="file" onchange="changeImg(this)"
-                                        class="image form-control-file" style="display: none;" value="">
+                                        class="image form-control-file" style="display: none;" value="" placeholder="Chỉ chấp nhận các file .pdf .jpg">
                                     <input type="hidden" name="image_old" value="">
                                 </div>
                             </div>
+
                             @error('name')
                                 <div class="alert alert-danger" role="alert">
                                     {{ $message }}
                                 </div>
                             @enderror
                             <div class="position-relative row form-group">
-                                <label for="name" class="col-md-3 text-md-right col-form-label">Name</label>
+                                <label for="name" class="col-md-3 text-md-right col-form-label">Họ và tên</label>
                                 <div class="col-md-9 col-xl-8">
-                                    <input name="name" id="name" placeholder="Name" type="text"
+                                    <input name="name" id="name" placeholder="VD: Nguyễn Văn A" type="text"
                                         class="form-control" value="">
                                 </div>
                             </div>
+
                             @error('email')
                                 <div class="alert alert-danger" role="alert">
                                     {{ $message }}
@@ -51,29 +54,31 @@
                                         class="form-control" value="">
                                 </div>
                             </div>
+
                             @error('password')
                                 <div class="alert alert-danger" role="alert">
                                     {{ $message }}
                                 </div>
                             @enderror
                             <div class="position-relative row form-group">
-                                <label for="password" class="col-md-3 text-md-right col-form-label">Password</label>
+                                <label for="password" class="col-md-3 text-md-right col-form-label">Mật khẩu</label>
                                 <div class="col-md-9 col-xl-8">
-                                    <input name="password" id="password" placeholder="Password" type="password"
+                                    <input name="password" id="password" placeholder="Mật khẩu" type="password"
                                         class="form-control" value="">
                                 </div>
                             </div>
-                            @error('password')
+
+                            @error('city')
                                 <div class="alert alert-danger" role="alert">
                                     {{ $message }}
                                 </div>
                             @enderror
                             <div class="position-relative row form-group">
                                 <label for="town_city" class="col-md-3 text-md-right col-form-label">
-                                    Town City
+                                    Thành phố
                                 </label>
                                 <div class="col-md-9 col-xl-8">
-                                    <input name="city" id="city" placeholder="City" type="text"
+                                    <input name="city" id="city" placeholder="Thành phố" type="text"
                                         class="form-control" value="">
                                 </div>
                             </div>
@@ -85,10 +90,10 @@
                             @enderror
                             <div class="position-relative row form-group">
                                 <label for="street_address" class="col-md-3 text-md-right col-form-label">
-                                    Street Address
+                                    Địa chỉ
                                 </label>
                                 <div class="col-md-9 col-xl-8">
-                                    <input name="street_address" id="street_address" placeholder="Street Address"
+                                    <input name="street_address" id="street_address" placeholder="VD: Tòa nhà Keangnam, Phạm Hùng, Cầu Giấy"
                                         type="text" class="form-control" value="">
                                 </div>
                             </div>
@@ -99,22 +104,23 @@
                                 </div>
                             @enderror
                             <div class="position-relative row form-group">
-                                <label for="phone" class="col-md-3 text-md-right col-form-label">Phone</label>
+                                <label for="phone" class="col-md-3 text-md-right col-form-label">Số điện thoại</label>
                                 <div class="col-md-9 col-xl-8">
-                                    <input name="phone" id="phone" placeholder="Phone" type="text"
+                                    <input name="phone" id="phone" placeholder="VD: 0123 456 789" type="text"
                                         class="form-control" value="">
                                 </div>
                             </div>
+
                             @error('level')
                                 <div class="alert alert-danger" role="alert">
                                     {{ $message }}
                                 </div>
                             @enderror
                             <div class="position-relative row form-group">
-                                <label for="level" class="col-md-3 text-md-right col-form-label">Level</label>
+                                <label for="level" class="col-md-3 text-md-right col-form-label">Vai trò</label>
                                 <div class="col-md-9 col-xl-8">
                                     <select name="level" id="level" class="form-control">
-                                        <option value="-1">-- Level --</option>
+                                        <option value="-1">-- Chọn vai trò --</option>
                                         @foreach (\App\Utilities\Constant::$user_status as $key => $value)
                                             <option value={{ $key }}>
                                                 {{ $value }}
@@ -124,27 +130,20 @@
                                 </div>
                             </div>
 
-                            {{-- <div class="position-relative row form-group">
-                                <label for="description" class="col-md-3 text-md-right col-form-label">Description</label>
-                                <div class="col-md-9 col-xl-8">
-                                    <textarea name="description" id="description" class="form-control"></textarea>
-                                </div>
-                            </div> --}}
-
                             <div class="position-relative row form-group mb-1">
                                 <div class="col-md-9 col-xl-8 offset-md-2">
                                     <a href="./admin/user" class="border-0 btn btn-outline-danger mr-1">
                                         <span class="btn-icon-wrapper pr-1 opacity-8">
                                             <i class="fa fa-times fa-w-20"></i>
                                         </span>
-                                        <span>Cancel</span>
+                                        <span>Hủy</span>
                                     </a>
 
                                     <button type="submit" class="btn-shadow btn-hover-shine btn btn-primary">
                                         <span class="btn-icon-wrapper pr-2 opacity-8">
                                             <i class="fa fa-download fa-w-20"></i>
                                         </span>
-                                        <span>Save</span>
+                                        <span>Thêm mới</span>
                                     </button>
                                 </div>
                             </div>
